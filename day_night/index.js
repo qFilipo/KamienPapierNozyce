7e2ef7f94d7614558ee00b47db9369967dd27635
Filip <rzymek29@interia.pl>
2023-12-16 14:01:48 +0100

    s

--- day_night/index.js
+++ day_night/index.js
@@ -18,15 +18,15 @@ document.addEventListener("DOMContentLoaded", function() {
 
 document.addEventListener("DOMContentLoaded", function() {
     const image = new Image();
-    image.src = "sun2.png";
+    image.src = "day_night/sun2.png";
     const image2 = new Image();
-    image2.src = "moon2.png"})
+    image2.src = "day_night/moon2.png"})
 
 const dayTime={
-    day: 'url("sun.png")',
-    day2: 'url("sun2.png")',
-    night: 'url("moon.png")',
-    night2: 'url("moon2.png")',
+    day: 'url("day_night/sun.png")',
+    day2: 'url("day_night/sun2.png")',
+    night: 'url("day_night/moon.png")',
+    night2: 'url("day_night/moon2.png")',
     filter:  'drop-shadow(0 0 0.3vw yellow)',
     filter2: 'none'
 }
@@ -37,7 +37,7 @@ function switchDayNight(){
     let backDiv = document.getElementById("day-night")
     let ball = document.getElementById("ball")
 
-    if (day.style.backgroundImage == 'url("sun2.png")'){
+    if (day.style.backgroundImage == dayTime.day2){
         day.style.backgroundImage = dayTime.day
         night.style.backgroundImage = dayTime.night
 
@@ -47,7 +47,6 @@ function switchDayNight(){
         ball.style.animation= "movie2 0.5s ease forwards"
 
         document.body.style.backgroundColor = "gray"
-        console.log("day")
     }
     else{
         day.style.backgroundImage = dayTime.day2
@@ -58,7 +57,6 @@ function switchDayNight(){
         ball.style.animation= "movie 0.5s ease forwards"
 
         document.body.style.backgroundColor = "black"
-        console.log("night")
     }
 
 }
